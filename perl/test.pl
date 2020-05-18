abdea70b41e21ae58e1b407a4a50268ea5abd8b5
Nelson do Vale Oliveira <nelson@ocastudios.com>
2020-05-17 21:23:49 -0300

    Syntax perl

---
+++ perl/test.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+
+use feature "switch";
+
+# Comment
+#
+
+print "Hello World\n";
+
+my $name = "Nelson do Vale Oliveira";
+my ($age, $address) = (40, "QSC 19");
+my $first_string = "Oi\ntudo bem?";
+my $second_string = 'Oi\ntudo bem?';
+my $complete_sentence = "My name is $name,\n I'm $age and I live in $address";
+
+print $complete_sentence;
+print int 7.5;
+
